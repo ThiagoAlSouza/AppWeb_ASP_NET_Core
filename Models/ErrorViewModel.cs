8b1c35db8a3e81ef91512f3292be9e26c3e162f7
Thiago Alves <ThiagoSouza98@outlook.com.br>
2021-11-18 19:07:34 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AppWeb_ASP_NET_Core.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
