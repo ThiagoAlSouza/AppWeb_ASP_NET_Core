e9621db25ce4dd8b55a27d5385535491c4322cde
Thiago Alves <ThiagoSouza98@outlook.com.br>
2021-11-18 19:12:28 -0300

    Tag Helper Customizada para Email

---
+++ TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppWeb_ASP_NET_Core.TagHelpers
+{
+    public class EmailTagHelper : TagHelper
+    {
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            var content = await output.GetChildContentAsync();
+            var target = content.GetContent() + "@" + "outlook.com";
+            output.Attributes.SetAttribute("href", "mailto" + target);
+            output.Content.SetContent(target);
+        }
+    }
+}
