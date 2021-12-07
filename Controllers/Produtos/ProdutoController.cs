676f67d2be4fdc1d839581d49261326c92a1d8cd
Thiago Alves <ThiagoSouza98@outlook.com.br>
2021-12-06 21:33:12 -0300

    Limpesa na Controller de Produto

--- Controllers/Produtos/ProdutoController.cs
+++ Controllers/Produtos/ProdutoController.cs
@@ -26,50 +26,13 @@ namespace AppWeb_ASP_NET_Core.Controllers.Produtos
             var produto = new Produto
             {
                 Nome = "Arroz",
-                Preco = 19.99,
+                Preco = 19.98,
                 Avaliacao = 10,
-                Validade = true
+                Validade = false
             };
 
             _context.Produtos.Add(produto);
             _context.SaveChanges();
-
-            //_context.Produtos.Remove(produto);
-            //_context.SaveChanges();
-
-            //var produtos = _context.Produtos.AsQueryable();
-
-            //foreach (var produto in produtos)
-            //{
-            //    if (produto.Preco > 19.98)
-            //    {
-            //        produto.Preco = 9.99;
-            //    }
-            //}
-
-            //_context.SaveChanges();
-
-            //var produtos = _context.Produtos.AsQueryable();
-
-            //foreach (var produto in produtos)
-            //{
-            //    produto.Validade = false;
-
-            //    _context.Produtos.Update(produto);
-            //}
-
-            //_context.Produtos.RemoveRange(produtos);
-
-            //_context.SaveChanges();
-
-            //if (produto3 != null)
-            //{
-            //    produto3.Id = Guid.NewGuid();
-            //    produto3.Nome = "Feijão";
-
-            //    _context.Produtos.Add(produto3);
-            //    _context.SaveChanges();
-            //}
 
             return View();
         }
